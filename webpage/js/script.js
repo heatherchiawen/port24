b1cc1964f80ae88785cfd6021783cd9d216effd5
Heather Chester <h_cheste@live.concordia.ca>
2024-04-08 21:22:54 -0400

    Added DOM material

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -14,4 +14,41 @@ setup();
 
 function setup() {
     console.log(document); 
+
+    // Get the <h1> element with id "main-heading" and store it in a variable 
+    let mainHeading = document.getElementById(`main-heading`); 
+
+    // Set its color property 
+    mainHeading.style[`color`] = `#339966`; 
+    mainHeading.style[`font-size`] = `5rem`; 
+    mainHeading.style[`font-family`] = `Helvetica, sans-serif`; 
+    mainHeading.style[`background-color`] = `#000000`; 
+
+    let pronoun = document.getElementById(`pronoun`); 
+    // pronoun.innerText = `you`; 
+    // if (pronoun.innerText === `we`) {
+    //     pronoun.innerText = `you`; 
+    // }
+    pronoun.innerHTML = "<strong>you<strong>"
+
+    // setting an attribute 
+    let image = document.getElementById(`clown-image`); 
+    // image.setAttribute(`src`, `http://loremflickr.com/320/240/clown`); 
+    // OR 
+    let src = image.getAttribute(`src`); 
+    if (src.includes(`clown`)) {
+        image.setAttribute(`src`, `http://loremflickr.com/320/240/clown`);
+    }
+
+    // More selection methods 
+    // Get all elements with class "header" on the page 
+    let headers = document.getElementByClassName(`header`);
+    // Loop through all the elements returned 
+    for (let i = 0; i < headers.length; i++) {
+        //set the color style property to red for each one 
+        headers[i],style.color = `#ff0000`; 
+    } 
+
+
+
 }
