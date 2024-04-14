45a1f68be31076224abfd6b8a092e5acfa371ef1
Heather Chester <h_cheste@live.concordia.ca>
2024-04-14 19:36:00 -0400

    Figured out hovering, tint, and text

--- FFAR-Final/js/script.js
+++ FFAR-Final/js/script.js
@@ -20,8 +20,11 @@
  * Description of preload
 */
 
-// Recipes 
-let bananaOne, bananaTwo, jamOne, jamTwo, chester, jerky; 
+// Sweet recipes 
+let bananaOne, bananaTwo, jamOne, jamTwo, chester; 
+
+// Savory recipes 
+let jerky, teaEgg; 
 
 // Photos for initial display 
 let cakePhoto, familyPhoto, shrinePhoto; 
@@ -29,13 +32,16 @@ let cakePhoto, familyPhoto, shrinePhoto;
 let state = `photoDisplay`; 
 
 function preload() {
-    // Loading recipes 
+    // Loading sweet recipes 
     bananaOne = loadImage(`assets/images/banana1.png`); 
     bananaTwo = loadImage(`assets/images/banana2.png`); 
     jamOne = loadImage(`assets/images/jam1.png`);
     jamTwo = loadImage(`assets/images/jam2.png`);  
     chester = loadImage(`assets/images/chester.png`); 
+
+    // Loading savory recipes 
     jerky = loadImage(`assets/images/jerky.png`); 
+    teaEgg = loadImage(`assets/images/teaEgg.png`); 
 
     // Loading photos for initial display 
     cakePhoto = loadImage(`assets/images/cake.png`); 
@@ -51,7 +57,6 @@ function setup() {
     createCanvas(windowWidth, windowHeight); 
 
     // Non-image settings 
-    textSize(32); 
     fill(0); 
     textFont(`Courier New`); 
     strokeWeight(3); 
@@ -73,56 +78,109 @@ function draw() {
     else if (state === `savorySection`) {
         savorySection(); 
     }
-
 }
 
 function photoDisplay() {
     // Text display 
     push();
+    textSize(32); 
     textAlign(CENTER, CENTER); 
-    text(`吃饱了, Chī bǎole, I am full`, width/2, 200);  
+    text(`吃饱了, Chī bǎole, I am full: an archive for family recipes`, width/2, 75);  
     textSize(14) 
-    text(`Heather Chester`, width/2, 250);  
+    text(`click images on the side for recipes`, width/2, 110);  
     // add descriptive subtext (MAYBE WITHIN THE SHRINE IMAGE???)
     pop();
 
     // Cake photo 
-    if (mouseX > width/2 - 500 && mouseX < width/2 - 500 + 350 && mouseY > height/2 && mouseY < height/2 + 250) {
-        tint(237, 213, 147);
-        image(cakePhoto, width/2 - 500, height/2, 350, 250);
+    if (mouseX > width/2 - 500 && mouseX < width/2 - 150 && mouseY > height/2 && mouseY < height/2 + 250) {
+        tint(227, 195, 207);
+        image(cakePhoto, width/2 - 600, height/2, 350, 250);
     }
     else {
         noTint(); 
-        image(cakePhoto, width/2 - 500, height/2, 350, 250);
+        image(cakePhoto, width/2 - 600, height/2, 350, 250);
     }  
 
     // Shrine photo display 
-    if (mouseX > width/2 - 100 && mouseX < width/2 - 100 + 250 && mouseY > height/2 - 50 && mouseY < height/2 - 50 + 350) {
+    if (mouseX > width/2 - 187.5 && mouseX < width/2 + 187.5 && mouseY > height/2 - 150 && mouseY < height/2 + 370) {
         tint(237, 213, 147);
-        image(shrinePhoto, width/2 - 100, height/2 - 50, 250, 350); 
+        image(shrinePhoto, width/2 - 187.5, height/2 - 150, 375, 520); 
     }
     else {
         noTint(); 
-        image(shrinePhoto, width/2 - 100, height/2 - 50, 250, 350);
+        image(shrinePhoto, width/2 - 187.5, height/2 - 150, 375, 520);
     }  
 
     // Family photo display 
-    if (mouseX > width/2 + 200 && mouseX < width/2 + 200 + 350 && mouseY > height/2 && mouseY < height/2 + 250) {
-        tint(237, 213, 147);
-        image(familyPhoto, width/2 + 200, height/2, 350, 250);
+    if (mouseX > width/2 + 250 && mouseX < width/2 + 600 && mouseY > height/2 && mouseY < height/2 + 250) {
+        tint(195, 201, 230); 
+        image(familyPhoto, width/2 + 250, height/2, 350, 250);
     }
     else {
         noTint();
-        image(familyPhoto, width/2 + 200, height/2, 350, 250);
+        image(familyPhoto, width/2 + 250, height/2, 350, 250);
     }
 }
 
 function dessertSection() {
     // Text display 
     push();
-    textAlign(CENTER, CENTER); 
-    text(`吃饱了, Chī bǎole, I am full`, width/2, 200);  
-    // add descriptive subtext (MAYBE WITHIN THE SHRINE IMAGE???) 
+    textSize(64); 
+    text(`sweet`, 20, 125);  
+    textSize(14);
+    text(`hover image to expand view\nclick to see back\n\n\non this page top to bottom:\n\n\nbanana bread\n\nchester cookies\n\njam drop cookies`, 40, 150); 
+    pop();
+
+    // Homing button 
+    strokeJoin(ROUND); 
+    line(30, 50, 45, 50); // bottom 
+    line(30, 50, 30, 40); // left eide 
+    line(30, 40, 37.5, 35); // left slant
+    line(37.5, 35, 45, 40); // right slant
+    line(45, 40, 45, 50); // right side
+    text(`home`, 55, 50);  
+
+    // Dessert image display 
+    noTint(); 
+    image(bananaOne, width/5, height/2 - 375, 170, 200);
+    image(chester, width/5, height/2 - 150, 170, 200); 
+    image(jamOne, width/5, height/2 + 75, 170, 300); 
+
+    // Banana check 
+    if (mouseX > width/5 && mouseX < width/5 + 170 && mouseY > height/2 - 375 && mouseY < height/2 - 175) {
+        if (mouseIsPressed === true) {
+            image(bananaTwo, width/2 - 100, height/2 - 300, 510, 600);
+        } else {
+            image(bananaOne, width/2 - 100, height/2 - 300, 510, 600);
+        }
+        tint(227, 195, 207); 
+        image(bananaOne, width/5, height/2 - 375, 170, 200);
+    } 
+    // chester cookies 
+    if (mouseX > width/5 && mouseX < width/5 + 170 && mouseY > height/2 - 150 && mouseY < height/2 + 50) {
+        image(chester, width/2 - 100, height/2 - 300, 510, 600);
+        tint(227, 195, 207);
+        image(chester, width/5, height/2 - 150, 170, 200);
+    } 
+    // Jam check 
+    if (mouseX > width/5 && mouseX < width/5 + 170 && mouseY > height/2 + 75 && mouseY < height/2 + 375) {
+        if (mouseIsPressed === true) {
+            image(jamTwo, width/2 - 100, height/2 - 375, 425, 750); 
+        } else {
+            image(jamOne, width/2 - 100, height/2 - 375, 425, 750);
+        }
+        tint(227, 195, 207); 
+        image(jamOne, width/5, height/2 + 75, 170, 300); 
+    } 
+}
+
+function savorySection() {
+    // Text display 
+    push();
+    textSize(64); 
+    text(`savory`, 20, 125);  
+    textSize(14);
+    text(`hover image to expand view\n\n\non this page:\n\n\njerky\n\ntea eggs`, 40, 160); 
     pop();
 
     // Homing button 
@@ -132,57 +190,42 @@ function dessertSection() {
     line(30, 40, 37.5, 35); // left slant
     line(37.5, 35, 45, 40); // right slant
     line(45, 40, 45, 50); // right side 
+    text(`home`, 55, 50); 
 
-    // Banana check 
-    if (mouseX > width/2 - 450 && mouseX < width/2 - 450 + 300 && mouseY > height/2 - 100 && mouseY < height/2 - 100 + 375) {
-        image(bananaTwo, width/2 - 450, height/2 - 100, 300, 375);
-    } else {
-        image(bananaOne, width/2 - 450, height/2 - 100, 300, 375); 
-    }
-    // HAVE MOUSEISPRESSED RESIZED OPTION?? 
-
-    // chester cookies 
-    image(chester, width/2 - 150, height/2 - 100, 300, 375);
-
-    // Jam check 
-    if (mouseX > width/2 + 150 && mouseX < width/2 + 150 + 300 && mouseY > height/2 - 100 && mouseY < height/2 - 100 + 375) {
-        image(jamTwo, width/2 + 150, height/2 - 100, 300, 375); 
-    }
-    else {
-        image(jamOne, width/2 + 150, height/2 - 100, 300, 375); 
-    }
-}
-
-function savorySection() {
-    // Homing button 
-    strokeJoin(ROUND); 
-    line(30, 50, 45, 50); // bottom 
-    line(30, 50, 30, 40); // left eide 
-    line(30, 40, 37.5, 35); // left slant
-    line(37.5, 35, 45, 40); // right slant
-    line(45, 40, 45, 50); // right side 
+    // Savory image display 
+    noTint(); 
+    image(jerky, width/2 - 200, height/2 + 125, 400, 200);  
+    image(teaEgg, width/2 + 250, height/2 + 125, 300, 200);
 
     // Jerky check 
-    if (mouseX > width/2 - 200 && mouseX < width/2 - 200 + 400 && mouseY > height/2 - 100 && mouseY < height/2 - 100 + 200) {
-        tint(237, 213, 147);
-        image(jerky, width/2 - 200, height/2 - 100, 400, 200);  
-    }
-    else {
-        noTint(); 
-        image(jerky, width/2 - 200, height/2 - 100, 400, 200);  
+    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 && mouseY > height/2 + 100 && mouseY < height/2 + 300)  {
+        image(jerky, width/2 - 400, height/2 - 300, 800, 400);  
+
+        tint(213, 216, 230);
+        image(jerky, width/2 - 200, height/2 + 125, 400, 200);  
+    } 
+    // teaEgg check 
+    if (mouseX > width/2 + 250 && mouseX < width/2 + 550 && mouseY > height/2 + 125 && mouseY < height/2 + 325) {
+        image(teaEgg, width/2 - 300, height/2 - 300, 600, 400);
+
+        tint(213, 216, 230); 
+        image(teaEgg, width/2 + 250, height/2 + 125, 300, 200);
     }
 }
 
 function mousePressed() {
-    if (state === `photoDisplay` && mouseX > width/2 - 500 && mouseX < width/2 - 500 + 350 && mouseY > height/2 && mouseY < height/2 + 250) {
+    // Check for if images in displayPhotos if pressed 
+    // cakePhoto
+    if (state === `photoDisplay` && mouseX > width/2 - 500 && mouseX < width/2 - 150 && mouseY > height/2 && mouseY < height/2 + 250) {
         state = `dessertSection`; 
     } 
-    else if (state === `photoDisplay` && mouseX > width/2 + 200 && mouseX < width/2 + 200 + 350 && mouseY > height/2 && mouseY < height/2 + 250) {
+    // familyPhoto 
+    else if (state === `photoDisplay` && mouseX > width/2 + 250 && mouseX < width/2 + 600 && mouseY > height/2 && mouseY < height/2 + 250) {
         state = `savorySection`; 
     }
 
     // Check for homing button 
-    if (state === `dessertSection` && mouseX > 30 && mouseX < 45 && mouseY > 35 && mouseY < 50) {   // || state === `savorySection`
+    if (state === `dessertSection` && mouseX > 30 && mouseX < 45 && mouseY > 35 && mouseY < 50) { 
         state = `photoDisplay`; 
     }
     else if (state === `savorySection` && mouseX > 30 && mouseX < 45 && mouseY > 35 && mouseY < 50) {
